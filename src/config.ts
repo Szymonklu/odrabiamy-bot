0e48c89d5728ad770a31560a8b731e9cf6944aa6
Szymonklu <103199723+Szymonklu@users.noreply.github.com>
2022-11-14 15:04:31 +0100

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export default {
-    token: '', // Discord bot token
+    token: 'MTA0MTcyNDY2MjA1ODU5MDI0OA.Glp20M.JE4vOPvjAihRK-47F5JX2Tk32Y_8Um_2-qsSj0', // Discord bot token
     channels: [ // Array of allowed channels IDs
         ''
     ],
